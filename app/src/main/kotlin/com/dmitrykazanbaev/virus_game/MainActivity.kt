3c9b9f9a2dba70cb4b2b92d66853cf47dbe2b281
dmitrykazanbaev <dmitry.kazanbaev@gmail.com>
2017-07-07 13:30:47 +0300

    added virus, statistics buttons

--- app/src/main/kotlin/com/dmitrykazanbaev/virus_game/MainActivity.kt
+++ app/src/main/kotlin/com/dmitrykazanbaev/virus_game/MainActivity.kt
@@ -2,19 +2,24 @@ package com.dmitrykazanbaev.virus_game
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
 import com.dmitrykazanbaev.virus_game.screen.FirstLevelView
 import com.dmitrykazanbaev.virus_game.service.ApplicationContextHolder
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         ApplicationContextHolder.context = this
 
         val firstLevelView = FirstLevelView(this)
         firstLevelView.holder.addCallback(firstLevelView)
 
-        setContentView(firstLevelView)
+        val frameLayout = findViewById(R.id.mainframe) as FrameLayout
+
+        frameLayout.addView(firstLevelView, 0)
     }
 }
